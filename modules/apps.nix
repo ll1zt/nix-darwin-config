0aa19fc71d0f2de13f67b114ac0913b69f663fc3
lllzt <lllzt@pm.me>
2024-05-04 11:15:47 +0800

    use yabai

--- modules/apps.nix
+++ modules/apps.nix
@@ -47,7 +47,7 @@
       #"curl" # no not install curl via nixpkgs, it's not working well on macOS!
       #"aria2" # download tool
       #"httpie" # http client
-      #"yabai"
+      "yabai"
       ];
 
     # `brew install --cask`

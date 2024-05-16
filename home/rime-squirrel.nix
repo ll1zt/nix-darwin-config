6ce0198574fbcbd22592b19fe56aab1e965a149a
lllzt <lllzt@pm.me>
2024-05-16 17:13:57 +0800

    fix

--- home/rime-squirrel.nix
+++ home/rime-squirrel.nix
@@ -1,29 +1,26 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.activation.copyRimeConfig = {
-    before = [ "linkGeneration" ];  # 确保在链接生成之前执行
-    text = let
-      # 你可以选择在Nix store中临时存储这个仓库或者直接在家目录下操作
-      # 这里使用的是直接在家目录下操作的方式
-      tempDir = "/Temporary/rime-config";
-    in ''
-      # 确保目标目录存在
-      mkdir -p $HOME/Library/Rime
+  home.activation.copyRimeConfig = lib.mkActivationScript "copyRimeConfig" ''
+    # 你可以选择在Nix store中临时存储这个仓库或者直接在家目录下操作
+    # 这里使用的是直接在家目录下操作的方式
+    tempDir="/Temporary/rime-config";
 
-      # 如果已经存在配置，可以选择备份或删除
-      if [ -d $HOME/Library/Rime ]; then
-        rm -rf $HOME/Library/Rime/*
-      fi
+    # 确保目标目录存在
+    mkdir -p "$HOME/Library/Rime";
 
-      # 克隆Git仓库
-      git clone https://github.com/ll1zt/Rime ${tempDir}
+    # 如果已经存在配置，可以选择备份或删除
+    if [ -d "$HOME/Library/Rime" ]; then
+      rm -rf "$HOME/Library/Rime"/*;
+    fi
 
-      # 复制文件
-      cp -r ${tempDir}/* $HOME/Library/Rime/
+    # 克隆Git仓库
+    git clone https://github.com/ll1zt/Rime "$tempDir";
 
-      # 清理临时目录
-      rm -rf ${tempDir}
-    '';
-  };
+    # 复制文件
+    cp -r "$tempDir"/* "$HOME/Library/Rime/";
+
+    # 清理临时目录
+    rm -rf "$tempDir";
+  '';
 }

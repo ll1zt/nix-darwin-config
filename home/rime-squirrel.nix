0436393fd96bff0fcdbcec695f10f909e9a6a1aa
lllzt <lllzt@pm.me>
2024-05-16 16:40:43 +0800

    .

--- home/rime-squirrel.nix
+++ home/rime-squirrel.nix
@@ -1,16 +1,14 @@
-{ pkgs, ... }:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
-  rime-data = pkgs.fetchFromGitHub {
-    owner = "ll1zt";
-    repo = "Rime";
-    rev = "main"; 
-    sha256 = "1fym7w9rh2wnq67pnxyp9znnl8cckcvj0c22b7rwwy7b2jmg0hxi";# 这里需要填入正确的哈希值
+  rimeSrc = pkgs.fetchGit {
+    url = "https://github.com/ll1zt/Rime";
+    rev = "main"; # 使用最新的 master 分支
   };
 in
 {
   home.file."Library/Rime" = {
-    source = rime-data;
+    source = rimeSrc;
     recursive = true;
     force = true;
   };

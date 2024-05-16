6023e10c4a04fa8249645645446deaca60ada7be
lllzt <lllzt@pm.me>
2024-05-16 16:35:51 +0800

    fix

--- home/rime-squirrel.nix
+++ home/rime-squirrel.nix
@@ -5,7 +5,7 @@ let
     owner = "ll1zt";
     repo = "Rime";
     rev = "main"; 
-    sha256 = "0000000000000000000000000000000000000000000000000000"; # 这里需要填入正确的哈希值
+    sha256 = "1fym7w9rh2wnq67pnxyp9znnl8cckcvj0c22b7rwwy7b2jmg0hxi";# 这里需要填入正确的哈希值
   };
 in
 {

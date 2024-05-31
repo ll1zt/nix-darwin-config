cc80cc1b6a97a01619a3dc21415c60bfdcf7298f
lllzt <lllzt@pm.me>
2024-05-31 10:21:12 +0800

    kitty setting

--- home/kitty.nix
+++ home/kitty.nix
@@ -29,25 +29,13 @@
         else 13;
     };
 
-    # consistent with wezterm
-#   keybindings = {
-#     "ctrl+shift+m" = "toggle_maximized";
-#     "ctrl+shift+f" = "show_scrollback"; # search in the current window
-#   };
 
     settings = {
       background_opacity = "0.93";
       macos_option_as_alt = true; # Option key acts as Alt on macOS
       enable_audio_bell = false;
       tab_bar_edge = "top"; # tab bar on top
-      #  To resolve issues:
-      #    1. https://github.com/ryan4yin/nix-config/issues/26
-      #    2. https://github.com/ryan4yin/nix-config/issues/8
-      #  Spawn a nushell in login mode via `bash`
-      shell = "${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'";
     };
 
-    # macOS specific settings
-    darwinLaunchOptions = ["--start-as=maximized"];
   };
 }

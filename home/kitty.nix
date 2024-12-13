d7a77c1f6f3959c4a79bf2a9b2f2f339f397bc2c
lllzt <lllzt@pm.me>
2024-12-13 08:21:05 +0800

    .

--- home/kitty.nix
+++ home/kitty.nix
@@ -31,7 +31,7 @@
 
 
     settings = {
-      background_opacity = "0.93";
+      background_opacity = "1";
       macos_option_as_alt = true; # Option key acts as Alt on macOS
       enable_audio_bell = false;
       tab_bar_edge = "top"; # tab bar on top

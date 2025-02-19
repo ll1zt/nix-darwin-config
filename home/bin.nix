b7f0d2b9420815627e0036adb8f5b2b99135c560
lllzt <lllzt@pm.me>
2025-02-19 16:49:07 +0800

    .

--- home/bin.nix
+++ home/bin.nix
@@ -3,7 +3,7 @@
 
 {
   home.packages = with pkgs; [
-    (writeShellScriptBin "on" (builtins.readFile ./on.sh)
+    (writeShellScriptBin "on" (builtins.readFile ./on.sh))
   ];
 
   # ... 你的其他 Home Manager 配置 ...

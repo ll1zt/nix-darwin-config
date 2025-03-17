9a257b6ad5731802fe72b6786cd8c13f047e4af9
lllzt <lllzt@pm.me>
2025-03-17 18:11:48 +0800

    .

--- modules/apps.nix
+++ modules/apps.nix
@@ -47,7 +47,7 @@
       "fd" #for searching files
       "ripgrep" # for searching files
       "clouddrive2"
-      "node"
+      #"node"
       "fzf" # for fuzzy search
       "bat" # for syntax highlighting
       "the_silver_searcher" #for

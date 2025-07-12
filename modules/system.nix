6dbd4a208f4bcfe472b876aaafc536e43b860d91
lllzt <lllzt@pm.me>
2025-07-12 15:52:09 +0800

    .

--- modules/system.nix
+++ modules/system.nix
@@ -3,13 +3,15 @@
 {
   ids.gids.nixbld = 30000; # set the nixbld group id to 30000
   system = {
-    stateVersion = 5;
+    primaryUser = "lllzt";
+    stateVersion = 25.05;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
+    activationScripts.postSystemActivation.text = '' # <--- CHANGED TO postSystemActivation
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
+      # Execute as the primary user:
+      sudo -u lllzt /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';    
 
     defaults = {
       # menuExtraClock.Show24Hour = true;  # show 24 hour clock
